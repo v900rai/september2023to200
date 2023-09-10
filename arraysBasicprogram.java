7cc08d129ecd689db36afc27dbc69de967b49357
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-09-11 00:22:24 +0530

    Add files via upload

---
+++ arraysBasicprogram.java
@@ -0,0 +1,20 @@
+package sept2023to200;
+
+public class arraysBasicprogram 
+{
+	public static void main(String []args)
+	{
+		int arr[]=new int[5];
+		arr[0]=1;
+		arr[1]=2;
+		arr[2]=3;
+		arr[3]=4;
+		arr[4]=5;
+		// printing the lengths
+		for(int i=0; i<arr.length; i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+
+}
