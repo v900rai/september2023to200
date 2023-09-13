9e361ec59b8dbeef5f4995d27eef33f3d1c5d1e6
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-09-13 14:07:45 +0530

    Add files via upload

---
+++ isEmpteyStackProgram.java
@@ -0,0 +1,16 @@
+package allmethodsInStack;
+
+import java.util.*;
+
+public class isEmpteyStackProgram
+{
+	public static void main(String []args)
+	{
+		Stack<Integer> st=new Stack<>();
+		st.push(10);
+		st.push(20);
+		System.out.println(st.isEmpty()); //true and fale is working
+		
+	}
+
+}
