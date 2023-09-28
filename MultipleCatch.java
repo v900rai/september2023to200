78880ede8b3ead9099740630c9db68c8703a31ad
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-09-28 11:20:44 +0530

    Add files via upload

---
+++ MultipleCatch.java
@@ -0,0 +1,24 @@
+package sept2023to200;
+
+public class MultipleCatch {
+	public static void main(String args[]) {
+		 try {
+		  int n = 1000, x = 0;
+		  int arr[] = new int[n];
+		  for (int i = 0; i <= n; i++) {
+		   arr[i] = i / x;
+		  }
+		 }
+		 catch (ArrayIndexOutOfBoundsException exception) {
+		  System.out.println("1st block = ArrayIndexOutOfBoundsException");
+		 }
+		 catch (ArithmeticException exception) {
+		  System.out.println("2nd block = ArithmeticException");
+		 }
+		 catch (Exception exception) {
+		  System.out.println("3rd block = Exception");
+		 }
+		}
+		}
+
+
