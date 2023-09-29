da682e6e365150b833317557493a50f9c5a866fa
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-09-29 23:31:17 +0530

    Add files via upload

---
+++ primeNumber.java
@@ -0,0 +1,22 @@
+package sept2023to200;
+
+public class primeNumber 
+{
+	public static void main(String []args)
+	{
+		int num=5;
+		int count=0;
+		for(int i=1; i<=num; i++)
+		
+			if(num%2==0)
+			{
+				System.out.println(" its prime number" +num);
+			}
+			else 
+			{
+				System.out.println(" its not prime number " +num);
+			}
+		
+	}
+
+}
