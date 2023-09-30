ea1464b43bcbcd753c5b3d42592bae50f4de1c96
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2023-09-30 12:53:26 +0530

    Add files via upload

---
+++ basicNew.java
@@ -0,0 +1,39 @@
+package sept2023to200;
+
+public class basicNew 
+{
+	// Java Program to demonstrate use of default valuess
+		// static values
+		static byte b;
+		static int i;
+		static long l;
+		static short s;
+		static boolean bool;
+		static char c;
+		static String str;
+		static Object object;
+		static float f;
+		static double d;
+		static int[] Arr;
+		public static void main(String[] args)
+		{
+			// byte value
+			System.out.println("byte value" + b);
+			// short value
+			System.out.println("short value" + s);
+			// int value
+			System.out.println("int value" + i);
+			// long value
+			System.out.println("long value" + l);
+			System.out.println("boolean value" + bool);
+			System.out.println("char value" + c);
+			System.out.println("float value" + f);
+			System.out.println("double value" + d);
+			System.out.println("string value" + str);
+			System.out.println("object value" + object);
+			System.out.println("Array value" + Arr);
+		}
+	}
+
+
+
